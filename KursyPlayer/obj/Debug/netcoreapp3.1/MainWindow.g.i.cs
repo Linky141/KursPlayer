e56a9d9232bbdd0208718ad6b38ea7d027c76aaf
Tomasz Bielas <tomaszbielas97@gmail.com>
2021-10-29 15:30:38 +0200

    icon

--- KursyPlayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ KursyPlayer/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -174,7 +174,7 @@ namespace KursyPlayer {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/KursyPlayer;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/KursyPlayer;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
